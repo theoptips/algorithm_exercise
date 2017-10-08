5a92b565b0330e182efb7229676956eecfa365b3
Yu Sun <ysun@Yus-MacBook-Air-2.local>
2017-10-08 16:58:59 -0700

    generate a large number case

--- large_num.py
+++ large_num.py
@@ -1,9 +1,15 @@
-import array
-a = array.array('I', xrange(10**8))
-import random
-random.shuffle(a)
+# import array
+# a = array.array('I', xrange(10**8))
+# import random
+# random.shuffle(a)
 
+
+
+
+
+a = [1]*10000000
+output = ' '.join(str(i) for i in a)
 
 text_file = open("Output.txt", "w")
-text_file.write(a)
+text_file.write(output)
 text_file.close()
